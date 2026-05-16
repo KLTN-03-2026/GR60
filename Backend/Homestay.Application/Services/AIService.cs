844ec540b2a325f62da2da99b7c353655c7e007d
nguyenhphuc2301 <nguyenhphuc2301@gmail.com>
2026-05-16 16:19:56 +0700

    3

--- Backend/Homestay.Application/Services/AIService.cs
+++ Backend/Homestay.Application/Services/AIService.cs
@@ -18,7 +18,7 @@ namespace Homestay.Application.Services
     {
         private HttpClient _httpClient;
         private IUnitOfWork _unitOfWork;
-        private string _api;
+        private string _aTK;
         private string _accountId;
         private string _model;
 
@@ -31,7 +31,7 @@ namespace Homestay.Application.Services
         public async Task<string> GetMessageAIResponse(NgayDuDoanRequest ngayDuDoanRequest, DataAIprice dataAIprice)
         {
             _httpClient = new HttpClient();
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _api);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _aTK);
             string endpoint = $"https://api.cloudflare.com/client/v4/accounts/{_accountId}/ai/run/{_model}";
 
 
